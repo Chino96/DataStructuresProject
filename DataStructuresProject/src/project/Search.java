0eae4fba45d2f6eac671fc29d9b477b42ab52e78
cu00393 <cu00393@CEIT2552212X030.ad.georgiasouthern.edu>
2018-04-10 13:47:18 -0400

    Using Java fx for text box

--- DataStructuresProject/src/project/Search.java
+++ DataStructuresProject/src/project/Search.java
@@ -2,10 +2,35 @@ package project;
 
 import java.util.ArrayList;
 
-public class Search{
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Search extends Application{
 
 	public static void main(String[] args) throws Exception {
+		launch();
+	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		Pane pane = new Pane();
+		Button search = new Button("Search");
+		TextArea textBox = new TextArea();
+		
+		
+		textBox.setStyle("-fx-font-size: 24");
+		textBox.setWrapText(true);
+
+		pane.getChildren().addAll(search, textBox);
+		
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+		stage.show();
+		
 		Dictionary dict = new Dictionary("Dict.txt");
 		Tree tree = new Tree();
 		dict.build(tree);
@@ -14,6 +39,7 @@ public class Search{
 
 		System.out.println(dict.getDeff(tree.search("Dog", arr).get(0)));
 
+		
 	}
 
 }
